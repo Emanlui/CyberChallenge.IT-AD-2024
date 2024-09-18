42cf0d13213ae9282864ddaaf539c9b9419aac62
Emanlui <emanuellejs1999@gmail.com>
2024-09-17 19:59:53 -0600

    Update start.sh

--- services/ExCCel/start.sh
+++ services/ExCCel/start.sh
@@ -4,4 +4,4 @@ FLASK_SECRET=$(xxd -u -l 16 -p /dev/urandom)
 sed -i "s/FLASK_SECRET: CHANGEME/FLASK_SECRET: $FLASK_SECRET/" docker-compose.yml
 
 chmod +x ./processor
-docker compose up -d --build
+docker-compose up -d --build
